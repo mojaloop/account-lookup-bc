74ff21cf2835ae50a82e96183e451ab7dcc0cf03
Pedro Sousa Barreto <pedrosousabarreto@gmail.com>
2022-10-20 19:08:22 +0100

    Removed dependency from domain, fixed some minor issues and formating.

--- packages/http-oracle-svc/src/server/remote_oracle_routes.ts
+++ packages/http-oracle-svc/src/server/remote_oracle_routes.ts
@@ -1,164 +1,168 @@
-import {IOracleProviderAdapter} from '@mojaloop/account-lookup-bc-domain';
 /**
- License
- --------------
- Copyright © 2021 Mojaloop Foundation
+License
+--------------
+Copyright © 2021 Mojaloop Foundation
 
- The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License.
+The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License.
 
- You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
- Contributors
- --------------
- This is the official list (alphabetical ordering) of the Mojaloop project contributors for this file.
- Names of the original copyright holders (individuals or organizations)
- should be listed with a '' in the first column. People who have
- contributed from an organization can be listed under the organization
- that actually holds the copyright for their contributions (see the
- Gates Foundation organization for an example). Those individuals should have
- their names indented and be marked with a '-'. Email address can be added
- optionally within square brackets <email>.
+Contributors
+--------------
+This is the official list (alphabetical ordering) of the Mojaloop project contributors for this file.
+Names of the original copyright holders (individuals or organizations)
+should be listed with a '' in the first column. People who have
+contributed from an organization can be listed under the organization
+that actually holds the copyright for their contributions (see the
+Gates Foundation organization for an example). Those individuals should have
+their names indented and be marked with a '-'. Email address can be added
+optionally within square brackets <email>.
 
- * Gates Foundation
- - Name Surname <name.surname@gatesfoundation.com>
+* Gates Foundation
+- Name Surname <name.surname@gatesfoundation.com>
 
- * Coil
- - Jason Bruwer <jason.bruwer@coil.com>
+* Coil
+- Jason Bruwer <jason.bruwer@coil.com>
 
- * Crosslake
- - Pedro Sousa Barreto <pedrob@crosslaketech.com>
+* Crosslake
+- Pedro Sousa Barreto <pedrob@crosslaketech.com>
 
- * Gonçalo Garcia <goncalogarcia99@gmail.com>
- 
- * Arg Software
- - José Antunes <jose.antunes@arg.software>
- - Rui Rocha <rui.rocha@arg.software>
+* Gonçalo Garcia <goncalogarcia99@gmail.com>
 
- --------------
- **/
+* Arg Software
+- José Antunes <jose.antunes@arg.software>
+- Rui Rocha <rui.rocha@arg.software>
 
- "use strict";
+--------------
+**/
+
+"use strict";
 
 
- import express from "express";
- import {ILogger} from "@mojaloop/logging-bc-public-types-lib";
- import { body, check, validationResult } from "express-validator";
-  
- 
- export class RemoteOracleExpressRoutes {
-     private readonly _logger: ILogger;
-     private readonly _oracle : IOracleProviderAdapter;
-     private mainRouter = express.Router();
- 
-     constructor(logger: ILogger, oracleAdapter: IOracleProviderAdapter) {
-         this._logger = logger;
-         this._oracle = oracleAdapter;
-         // http oracle routes
+import express from "express";
+import {ILogger} from "@mojaloop/logging-bc-public-types-lib";
+import {body, check, validationResult} from "express-validator";
+import {RemoteOracle} from "../remote_oracle";
 
-         this.mainRouter.get("/health",this.healthCheck.bind(this));
 
-         this.mainRouter.get("/participants/:partyType/:partyId/:partySubId?",[
-                check("partyType").isString().notEmpty().withMessage(" partyType is required").bail(),
-                check("partyId").isString().notEmpty().withMessage(" partyId is required").bail(),
-                check("partySubId").optional(),
-            ], this.getParticipantFspId.bind(this));
-         
-         this.mainRouter.delete("/participants/:partyType/:partyId/:partySubId?",[
+export class RemoteOracleExpressRoutes {
+    private readonly _logger: ILogger;
+    private readonly _oracle: RemoteOracle;
+    private mainRouter = express.Router();
+
+    constructor(logger: ILogger, oracleAdapter: RemoteOracle) {
+        this._logger = logger;
+        this._oracle = oracleAdapter;
+        // http oracle routes
+
+        this.mainRouter.get("/health", this.healthCheck.bind(this));
+
+        this.mainRouter.get("/participants/:partyType/:partyId/:partySubId?", [
+            check("partyType").isString().notEmpty().withMessage(" partyType is required").bail(),
+            check("partyId").isString().notEmpty().withMessage(" partyId is required").bail(),
+            check("partySubId").optional(),
+        ], this.getParticipantFspId.bind(this));
+
+        this.mainRouter.delete("/participants/:partyType/:partyId/:partySubId?", [
             check("partyType").isString().notEmpty().withMessage(" partyType is required").bail(),
             check("partyId").isString().notEmpty().withMessage(" partyId is required").bail(),
             body("fspId").isString().notEmpty().withMessage(" fspId is required").bail(),
             check("partySubId").optional(),
-         ], this.disassociateParticipant.bind(this));
-         
-         this.mainRouter.post("/participants/:partyType/:partyId/:partySubId?",[
+        ], this.disassociateParticipant.bind(this));
+
+        this.mainRouter.post("/participants/:partyType/:partyId/:partySubId?", [
             check("partyType").isString().notEmpty().withMessage(" partyType is required").bail(),
             check("partyId").isString().notEmpty().withMessage(" partyId is required").bail(),
             body("fspId").isString().notEmpty().withMessage(" fspId is required").bail(),
             check("partySubId").optional(),
         ], this.associateParticipant.bind(this));
- 
-     }
- 
-     get MainRouter(): express.Router {
-         return this.mainRouter;
-     }
-     
-     private validateRequest(req: express.Request, res: express.Response<any, Record<string, any>>) : boolean {
+
+    }
+
+    get MainRouter(): express.Router {
+        return this.mainRouter;
+    }
+
+    private validateRequest(req: express.Request, res: express.Response<any, Record<string, any>>): boolean {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            res.status(422).json({ errors: errors.array() });
+            res.status(422).json({errors: errors.array()});
             return false;
         }
         return true;
     }
 
-    private extractRequestParams(req:express.Request) {
+    private extractRequestParams(req: express.Request) {
         const partyType = req.params["partyType"];
         const partyId = req.params["partyId"];
         const partySubId = req.params["partySubId"] ?? null;
         const currency = req.query["currency"]?.toString() ?? null;
         const fspId = req.body["fspId"] ?? null;
-        return { partyType, partyId, partySubId, currency,fspId };
+        return {partyType, partyId, partySubId, currency, fspId};
     }
 
 
-     private async getParticipantFspId(req: express.Request, res: express.Response, next: express.NextFunction) {
+    private async getParticipantFspId(req: express.Request, res: express.Response, next: express.NextFunction) {
         if (!this.validateRequest(req, res)) {
             return;
-        }             
+        }
         const params = this.extractRequestParams(req);
 
         this._logger.debug(`Fetching Participant FSP ID  with params [${params}].`);
 
-         try {
-             const fetched = await this._oracle.getParticipantFspId(params.partyType, params.partyId, params.partySubId, params.currency);
-             this._logger.debug(`Fetched Participant FSP ID [${fetched}] with params [${params}].`);
-             res.send(fetched);
-         } catch (err: any) {
+        try {
+            const fetched = await this._oracle.getParticipantFspId(params.partyType, params.partyId, params.partySubId, params.currency);
+            this._logger.debug(`Fetched Participant FSP ID [${fetched}] with params [${params}].`);
+            if(!fetched){
+                res.sendStatus(404);
+            }else{
+                res.send(fetched);
+            }
+        } catch (err: any) {
             this._logger.error(`Error Fetching FSP ID with params [${params}]. -${err}`);
-             res.status(500).json({
-                 status: "error",
-                 msg: err.message
-             });
-         }
-     }
+            res.status(500).json({
+                status: "error",
+                msg: err.message
+            });
+        }
+    }
 
 
-     private async disassociateParticipant(req: express.Request, res: express.Response, next: express.NextFunction) {
-         if (!this.validateRequest(req, res)) {
-             return;
-         }
-        
-         const params = this.extractRequestParams(req);
-         this._logger.debug(`Disassociating Participant with params [${params}].`);
-
-         try {
-             const fetched = await this._oracle.disassociateParticipant(params.fspId, params.partyType, params.partyId, params.partySubId, params.currency);
-             this._logger.debug(`Disassociated Participant with params [${params}].`);           
-             res.send(null);
-         } catch (err: any) {
-             this._logger.error(`Error disassociating Participant with params [${params}]. -${err}`);
-             res.status(500).json({
-                 status: "error",
-                 msg: err.message
-             });
-         }
-     }
-
-     private async associateParticipant(req: express.Request, res: express.Response, next: express.NextFunction) {
+    private async disassociateParticipant(req: express.Request, res: express.Response, next: express.NextFunction) {
         if (!this.validateRequest(req, res)) {
             return;
         }
-       
+
+        const params = this.extractRequestParams(req);
+        this._logger.debug(`Disassociating Participant with params [${params}].`);
+
+        try {
+            await this._oracle.disassociateParticipant(params.fspId, params.partyType, params.partyId, params.partySubId, params.currency);
+            this._logger.debug(`Disassociated Participant with params [${params}].`);
+            res.send(null);
+        } catch (err: any) {
+            this._logger.error(`Error disassociating Participant with params [${params}]. -${err}`);
+            res.status(500).json({
+                status: "error",
+                msg: err.message
+            });
+        }
+    }
+
+    private async associateParticipant(req: express.Request, res: express.Response, next: express.NextFunction) {
+        if (!this.validateRequest(req, res)) {
+            return;
+        }
+
         const params = this.extractRequestParams(req);
         this._logger.debug(`Associating Participant with params [${params}].`);
 
         try {
-            const fetched = await this._oracle.associateParticipant(params.fspId, params.partyType, params.partyId, params.partySubId, params.currency);
+            await this._oracle.associateParticipant(params.fspId, params.partyType, params.partyId, params.partySubId, params.currency);
             this._logger.debug(`Associated Participant with params [${params}].`);
-            res.send(fetched);
+            res.send();
         } catch (err: any) {
             this._logger.error(`Error associating Participant with params [${params}]. -${err}`);
             res.status(500).json({
@@ -167,8 +171,8 @@ import {IOracleProviderAdapter} from '@mojaloop/account-lookup-bc-domain';
             });
         }
     }
-     
-     private async healthCheck(req: express.Request, res: express.Response, next: express.NextFunction) {
+
+    private async healthCheck(req: express.Request, res: express.Response, next: express.NextFunction) {
         try {
             this._logger.debug(`Health Check.`);
             const fetched = await this._oracle.healthCheck();
@@ -181,6 +185,6 @@ import {IOracleProviderAdapter} from '@mojaloop/account-lookup-bc-domain';
                 msg: err.message
             });
         }
-    } 
- 
- }
+    }
+
+}
--- packages/http-oracle-svc/src/service.ts
+++ packages/http-oracle-svc/src/service.ts
@@ -1,64 +1,67 @@
 /**
- License
- --------------
- Copyright © 2021 Mojaloop Foundation
+License
+--------------
+Copyright © 2021 Mojaloop Foundation
 
- The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License.
+The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License.
 
- You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
- Contributors
- --------------
- This is the official list (alphabetical ordering) of the Mojaloop project contributors for this file.
- Names of the original copyright holders (individuals or organizations)
- should be listed with a '' in the first column. People who have
- contributed from an organization can be listed under the organization
- that actually holds the copyright for their contributions (see the
- Gates Foundation organization for an example). Those individuals should have
- their names indented and be marked with a '-'. Email address can be added
- optionally within square brackets <email>.
+Contributors
+--------------
+This is the official list (alphabetical ordering) of the Mojaloop project contributors for this file.
+Names of the original copyright holders (individuals or organizations)
+should be listed with a '' in the first column. People who have
+contributed from an organization can be listed under the organization
+that actually holds the copyright for their contributions (see the
+Gates Foundation organization for an example). Those individuals should have
+their names indented and be marked with a '-'. Email address can be added
+optionally within square brackets <email>.
 
- * Gates Foundation
- - Name Surname <name.surname@gatesfoundation.com>
+* Gates Foundation
+- Name Surname <name.surname@gatesfoundation.com>
 
- * Coil
- - Jason Bruwer <jason.bruwer@coil.com>
+* Coil
+- Jason Bruwer <jason.bruwer@coil.com>
 
- * Crosslake
- - Pedro Sousa Barreto <pedrob@crosslaketech.com>
+* Crosslake
+- Pedro Sousa Barreto <pedrob@crosslaketech.com>
 
- * Gonçalo Garcia <goncalogarcia99@gmail.com>
- 
- * Arg Software
- - José Antunes <jose.antunes@arg.software>
- - Rui Rocha <rui.rocha@arg.software>
+* Gonçalo Garcia <goncalogarcia99@gmail.com>
 
- --------------
- **/
+* Arg Software
+- José Antunes <jose.antunes@arg.software>
+- Rui Rocha <rui.rocha@arg.software>
 
- "use strict";
+--------------
+**/
 
-import { IOracleProviderAdapter } from "@mojaloop/account-lookup-bc-domain";
-import { ConsoleLogger, ILogger } from "@mojaloop/logging-bc-public-types-lib";
+"use strict";
+
+import { ILogger } from "@mojaloop/logging-bc-public-types-lib";
 import express, {Express} from "express";
 import { Server } from "net";
 import { RemoteOracle } from "./remote_oracle";
 import { RemoteOracleExpressRoutes } from "./server/remote_oracle_routes";
- 
+import {DefaultLogger} from "@mojaloop/logging-bc-client-lib";
 
- // Oracle server
+// Oracle server
+const BC_NAME = "account-lookup-bc";
+const APP_NAME = "http-oracle-svc";
+const APP_VERSION = "0.0.1";
+
 const REMOTE_ORACLE_PORT = process.env["REMOTE_ORACLE_PORT"] || 3031;
-const FILE_PATH = process.env["ORACLE_FILE"] || "db.json";
+const FILE_PATH = process.env["ORACLE_FILE"] || "./dist/db.json";
 let logger: ILogger;
 let expressApp: Express;
 let remoteOracleServer: Server;
-let remoteOracle: IOracleProviderAdapter;
+let remoteOracle: RemoteOracle;
 
 export async function start(){
     try{
-        logger = new ConsoleLogger();
+        logger = new DefaultLogger(BC_NAME, APP_NAME, APP_VERSION);
         remoteOracle = new RemoteOracle(FILE_PATH, logger);
         await remoteOracle.init();
 
@@ -72,13 +75,13 @@ export async function start(){
         expressApp.use("/remote-oracle", routes.MainRouter);
 
         expressApp.use((req, res) => {
-        // catch all
-        res.send(404);
+            // catch all
+            res.send(404);
         });
 
         remoteOracleServer = expressApp.listen(REMOTE_ORACLE_PORT, () => {
-        logger.info(`🚀 Server ready at: http://localhost:${REMOTE_ORACLE_PORT}`);
-        logger.info("Remote Oracle Server started");
+            logger.info(`🚀 Server ready at: http://localhost:${REMOTE_ORACLE_PORT}`);
+            logger.info("Remote Oracle Server started");
         });
 
     }
